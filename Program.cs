d5ffc745e6ff5b03f9bddce637fe68d369b3fe79
Lorenzo Perassi <perassi.lorenzo1804@gmail.com>
2026-07-16 12:22:13 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace fileDropbox
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            fileDropbox applicativo = new fileDropbox();
+            applicativo.Run();
+        }
+    }
+}
